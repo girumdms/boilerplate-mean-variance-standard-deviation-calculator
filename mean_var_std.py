7dc5d1db87d4cf6d8c0a3628fd4ebf1792d57562
girumdms <164764083+girumdms@users.noreply.github.com>
2024-03-26 16:12:19 +0000

    initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
-    return calculations
+def calculate(x):
+    v = np.array([x])
+    if v.size < 9:
+        return "valueError => list must contain nine numbers"
+    else:
+        w = v.reshape(3,3)
+        fg_mean = np.mean(w, axis=0).tolist(), np.mean(w, axis=1).tolist(), np.mean(v, axis=1).tolist()
+        fg_var = np.var(w, axis=0).tolist(), np.var(w, axis=1).tolist(), np.var(v, axis=1).tolist()
+        fg_std = np.std(w, axis=0).tolist(), np.std(w, axis=1).tolist(), np.std(v, axis=1).tolist()
+        fg_max = np.max(w, axis=0).tolist(), np.max(w, axis=1).tolist(), np.max(v, axis=1).tolist()
+        fg_min = np.min(w, axis=0).tolist(), np.min(w, axis=1).tolist(), np.min(v, axis=1).tolist()
+        fg_sum = np.sum(w, axis=0).tolist(), np.sum(w, axis=1).tolist(), np.sum(v, axis=1).tolist()
+        calculations = {'mean' :fg_mean, 'variance' : (fg_var), 'standard deviation': (fg_std),
+                        'max': (fg_max), 'min':(fg_min), 'sum': (fg_sum)}
+        return calculations
